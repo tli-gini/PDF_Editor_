92051ab460fc3197a27cf8e2b6703f464430c26c
tli-gini <ginitong.fish0524@gmail.com>
2025-07-08 14:28:37 +0800

    fix basic setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{css,scss}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
