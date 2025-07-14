f716dbc167ef1767e464846c6e0b37b07b05bcb6
tli-gini <ginitong.fish0524@gmail.com>
2025-07-14 15:22:49 +0800

    checkbox-add borders

--- app/(tools)/structure/multi-page/page.tsx
+++ app/(tools)/structure/multi-page/page.tsx
@@ -1,14 +1,18 @@
 // app/(tools)/structure/multi-page/page.tsx
 "use client";
+
+import { useState } from "react";
 import { useI18n } from "@/lib/i18n-context";
 import DropzoneCard from "@/components/DropzoneCard";
 import ToolTitle from "@/components/ToolTitle";
+import CheckboxOption from "@/components/CheckboxOption";
 import SendButton from "@/components/SendButton";
 import ToolPageWrapper from "@/components/ToolPageWrapper";
 import { TbLayoutBoard } from "react-icons/tb";
 
 export default function MultiPage() {
   const { t } = useI18n();
+  const [addBorder, setAddBorder] = useState(false);
 
   return (
     <ToolPageWrapper>
@@ -16,8 +20,15 @@ export default function MultiPage() {
         icon={<TbLayoutBoard className="text-3xl" />}
         label={t.tools["multi-page"].label}
       />
-
       <DropzoneCard />
+      <CheckboxOption
+        id="remove-signature"
+        checked={addBorder}
+        onChange={setAddBorder}
+        labelKey="multi-page"
+        labelPath="checkboxLabel"
+      />
+
       <SendButton />
     </ToolPageWrapper>
   );
